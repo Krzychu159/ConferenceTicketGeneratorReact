ee211294f3df514d4ea4bf8ae95503e4fb97ce0c
Krzychu159 <krzysztof.rak159@gmail.com>
2025-03-27 20:51:10 +0100

    Update

--- src/components/ReadyTicket.jsx
+++ src/components/ReadyTicket.jsx
@@ -1,48 +1,63 @@
-function TicketForm() {
+function ReadyTicket({ name, email, git, file, onBack }) {
   return (
-    <section class="final">
-      <div class="logo">
-        <img src="assets/images/logo-full.svg" alt="" />
-      </div>
-      <div class="header">
-        <h1>
-          Congrats, <span id="name"></span> ! Your ticket is ready.
-        </h1>
-        <div class="inTicket">
-          <h2>
-            We've emailed your ticket to <span id="mail"></span> and will send
-            updates in the run up to the event.
-          </h2>
+    <div>
+      <section class="final">
+        <div class="logo">
+          <img src="assets/images/logo-full.svg" alt="" />
         </div>
-      </div>
-      <div class="ticket">
-        <div class="mainTicket">
-          <div class="headerTicket">
-            <div>
-              <img src="assets/images/logo-mark.svg" alt="logo" />
-            </div>
-            <div class="">
-              <div class="nameEvent">Coding Conf</div>
-              <div class="dateEvent">Jan 31, 2025 / Austin, TX</div>
-            </div>
+        <div class="header">
+          <h1>
+            Congrats,{" "}
+            <span style={{ color: "hsl(7, 88%, 67%)" }}> {name} </span> ! Your
+            ticket is ready.
+          </h1>
+          <div class="inTicket">
+            <h2>
+              We've emailed your ticket to{" "}
+              <span style={{ color: "hsl(7, 88%, 67%)" }}> {email} </span>
+              and will send updates in the run up to the event.
+            </h2>
           </div>
-          <div class="userDate">
-            <div>
-              <img src="assets/images/image-avatar.jpg" alt="" />
+        </div>
+        <div class="ticket">
+          <div class="mainTicket">
+            <div class="headerTicket">
+              <div>
+                <img src="assets/images/logo-mark.svg" alt="logo" />
+              </div>
+              <div class="">
+                <div class="nameEvent">Coding Conf</div>
+                <div class="dateEvent">Jan 31, 2025 / Austin, TX</div>
+              </div>
             </div>
-            <div class="nameMail">
-              <div class="fullname">XXXXXXX</div>
-              <div class="git">
-                <img src="assets/images/icon-github.svg" alt="git" />
-                <div>@XXXXXXX</div>
+            <div class="userDate">
+              <div>
+                {file ? (
+                  <img src={URL.createObjectURL(file)} alt="User Avatar" />
+                ) : (
+                  <img
+                    src="assets/images/image-avatar.jpg"
+                    alt="Default Avatar"
+                  />
+                )}
+              </div>
+              <div class="nameMail">
+                <div class="fullname">{name}</div>
+                <div class="git">
+                  <img src="assets/images/icon-github.svg" alt="git" />
+                  <div>@{git}</div>
+                </div>
               </div>
             </div>
           </div>
+          <div class="numberTicket">#6521</div>
         </div>
-        <div class="numberTicket">#XXXX</div>
+      </section>
+      <div className="backButton">
+        <button onClick={onBack}>Come back to form</button>
       </div>
-    </section>
+    </div>
   );
 }
 
-export default TicketForm;
+export default ReadyTicket;
--- src/components/TicketForm.jsx
+++ src/components/TicketForm.jsx
@@ -1,4 +1,26 @@
+import { useState } from "react";
+import ReadyTicket from "./ReadyTicket";
+
 function TicketForm() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [git, setGit] = useState("");
+  const [file, setFile] = useState("");
+
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  if (isSubmitted) {
+    return (
+      <ReadyTicket
+        name={name}
+        email={email}
+        git={git}
+        file={file}
+        onBack={() => setIsSubmitted(false)}
+      />
+    );
+  }
+
   return (
     <section className="form">
       <div className="logo">
@@ -8,23 +30,45 @@ function TicketForm() {
         <h1>Your Journey to Coding Conf 2025 Starts Here!</h1>
         <h2>Secure your spot at next year's biggest coding conference.</h2>
       </div>
-      <form>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          setIsSubmitted(true);
+        }}
+      >
         <label>Upload Avatar</label>
         <label htmlFor="file-upload" className="custom-file-upload">
           <img src="/assets/images/icon-upload.svg" alt="Upload" />
           Drag and drop or click to upload
         </label>
-        <input type="file" id="file-upload" />
+        <input
+          type="file"
+          id="file-upload"
+          accept="image/*"
+          onChange={(e) => {
+            setFile(e.target.files[0]);
+          }}
+        />
 
         <p>Upload your photo (JPG or PNG, max size: 500KB).</p>
         <label htmlFor="name">Full Name</label>
-        <input type="text" name="name" id="name" />
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
         <label htmlFor="email">E-mail Address</label>
         <input
           type="email"
           name="email"
           id="email"
           placeholder="example@email.com"
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
         />
         <label htmlFor="github">GitHub Username</label>
         <input
@@ -32,8 +76,11 @@ function TicketForm() {
           name="github"
           id="github"
           placeholder="@yourusername"
+          onChange={(e) => {
+            setGit(e.target.value);
+          }}
         />
-        <button>Generate My Ticket</button>
+        <button className="backButton">Generate My Ticket</button>
       </form>
     </section>
   );
